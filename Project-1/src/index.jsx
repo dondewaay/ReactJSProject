6854ad6bc1eb1fefaccbc39536190df44cd58873
ddewaay <ddewaay@uiowa.edu>
2024-01-31 19:55:33 -0600

    Begin

---
+++ Project-1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Greeting = () => <h2>My First Component</h2>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);

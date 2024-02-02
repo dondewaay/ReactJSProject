75377421b4bedb1e80fd5c645911c7677513bf4d
ddewaay <ddewaay@uiowa.edu>
2024-02-02 16:51:22 -0600

    Figuring things out

--- Project-1/src/index.jsx
+++ Project-1/src/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const Greeting = () => <h2>My First Component</h2>;
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<Greeting />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
